909a164ce451cd3cf7a6bbc9f0d2e36ba9c1e2a8
Tyler Long <tyler4long@gmail.com>
2015-05-16 21:02:54 +0800

    Add FontAwesome.otf

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,22 @@
 from fabric.api import local
 
 def update():
-    local('rm -f FontAwesome.otf')
-    local('wget https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/FontAwesome.otf')
+    local('wget https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/FontAwesome.otf -O FontAwesome.otf')
+
+    local('wget https://github.com/FortAwesome/Font-Awesome/raw/master/src/icons.yml -O icons.yml')
+    file = open('icons.yml', 'r')
+    data = file.read()
+    file.close()
+    local('rm icons.yml')
+
+    import yaml
+    icons = yaml.load(data)['icons']
+    str = ''
+    for icon in icons:
+        str += '"{0}": "\u{{{1}}}",\n'.format(icon['id'], icon['unicode'])
+    swift_code = """icons = [
+{0}
+]""".format(str[0:-2])
+    swift_file = open('FontAwesome.swift', 'w')
+    swift_file.write(swift_code)
+    swift_file.close()
